1bb6257c9de30ee9206948dea8de2802c0bfaf06
hiuhihi78 <leduchieu2001x@gmail.com>
2023-06-25 10:04:37 +0700

    add payment with VNPay

---
+++ CinemaManagement/CinemaWebClient/Pages/Payment/Confirm.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using CinemaWebClient.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CinemaWebClient.Pages.Payment
+{
+    public class ConfirmModel : PageModel
+    {
+        [BindProperty]
+        public string Message { get; set; }
+
+        public IActionResult OnGet()
+        {
+            if (Request.QueryString.HasValue)
+            {
+                string hashSecret = "BDOQUQSZFZWMZUMEYYPTBMVDSKWUENYL";
+                var vnpayData = HttpContext.Request.Query;
+                PayLib pay = new PayLib();
+
+                //lấy toàn bộ dữ liệu được trả về
+                foreach (var parameter in vnpayData)
+                {
+                    if (parameter.Key.StartsWith("vnp_"))
+                    {
+                        pay.AddResponseData(parameter.Key, parameter.Value);
+                    }
+                }
+
+                long orderId = Convert.ToInt64(pay.GetResponseData("vnp_TxnRef")); //mã hóa đơn
+                long vnpayTranId = Convert.ToInt64(pay.GetResponseData("vnp_TransactionNo")); //mã giao dịch tại hệ thống VNPAY
+                string vnp_ResponseCode = pay.GetResponseData("vnp_ResponseCode"); //response code: 00 - thành công, khác 00 - xem thêm https://sandbox.vnpayment.vn/apis/docs/bang-ma-loi/
+                string vnp_SecureHash = Request.Query["vnp_SecureHash"]; //hash của dữ liệu trả về
+
+                bool checkSignature = pay.ValidateSignature(vnp_SecureHash, hashSecret); //check chữ ký đúng hay không?
+
+                if (checkSignature)
+                {
+                    if (vnp_ResponseCode == "00")
+                    {
+                        //Thanh toán thành công
+                        Message = "Thanh toán thành công hóa đơn " + orderId + " | Mã giao dịch: " + vnpayTranId;
+                    }
+                    else
+                    {
+                        //Thanh toán không thành công. Mã lỗi: vnp_ResponseCode
+                        Message = "Có lỗi xảy ra trong quá trình xử lý hóa đơn " + orderId + " | Mã giao dịch: " + vnpayTranId + " | Mã lỗi: " + vnp_ResponseCode;
+                    }
+                }
+                else
+                {
+                    Message = "Có lỗi xảy ra trong quá trình xử lý";
+                }
+            }
+
+            return Page();
+        }
+    }
+}
---
+++ CinemaManagement/CinemaWebClient/Pages/Payment/Index.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using CinemaWebClient.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
+
+namespace CinemaWebClient.Pages.Payment
+{
+    public class IndexModel : PageModel
+    {
+        public IActionResult OnGet(float amount)
+        {
+            string url = "http://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+            string returnUrl = "http://localhost:5006/Payment/Confirm";
+            string tmnCode = "ZY2FQM45";
+            string hashSecret = "BDOQUQSZFZWMZUMEYYPTBMVDSKWUENYL";
+
+            PayLib pay = new PayLib();
+
+            pay.AddRequestData("vnp_Version", "2.1.0"); //Phiên bản api mà merchant kết nối. Phiên bản hiện tại là 2.1.0
+            pay.AddRequestData("vnp_Command", "pay"); //Mã API sử dụng, mã cho giao dịch thanh toán là 'pay'
+            pay.AddRequestData("vnp_TmnCode", tmnCode); //Mã website của merchant trên hệ thống của VNPAY (khi đăng ký tài khoản sẽ có trong mail VNPAY gửi về)
+            pay.AddRequestData("vnp_Amount", "1000000"); //số tiền cần thanh toán, công thức: số tiền * 100 - ví dụ 10.000 (mười nghìn đồng) --> 1000000
+            pay.AddRequestData("vnp_BankCode", "NCB"); //Mã Ngân hàng thanh toán (tham khảo: https://sandbox.vnpayment.vn/apis/danh-sach-ngan-hang/), có thể để trống, người dùng có thể chọn trên cổng thanh toán VNPAY
+            pay.AddRequestData("vnp_CreateDate", DateTime.Now.ToString("yyyyMMddHHmmss")); //ngày thanh toán theo định dạng yyyyMMddHHmmss
+            pay.AddRequestData("vnp_CurrCode", "VND"); //Đơn vị tiền tệ sử dụng thanh toán. Hiện tại chỉ hỗ trợ VND
+            pay.AddRequestData("vnp_IpAddr", GetClientIPAddressRequested()); //Địa chỉ IP của khách hàng thực hiện giao dịch
+            pay.AddRequestData("vnp_Locale", "vn"); //Ngôn ngữ giao diện hiển thị - Tiếng Việt (vn), Tiếng Anh (en)
+            pay.AddRequestData("vnp_OrderInfo", "Thanh toan don hang"); //Thông tin mô tả nội dung thanh toán
+            pay.AddRequestData("vnp_OrderType", "other"); //topup: Nạp tiền điện thoại - billpayment: Thanh toán hóa đơn - fashion: Thời trang - other: Thanh toán trực tuyến
+            pay.AddRequestData("vnp_ReturnUrl", returnUrl); //URL thông báo kết quả giao dịch khi Khách hàng kết thúc thanh toán
+            pay.AddRequestData("vnp_TxnRef", DateTime.Now.Ticks.ToString()); //mã hóa đơn
+
+            string paymentUrl = pay.CreateRequestUrl(url, hashSecret);
+
+            return Redirect(paymentUrl);
+        }
+
+        private string GetClientIPAddressRequested()
+        {
+            IPAddress ipAddress;
+            var headers = HttpContext.Request.Headers.ToList();
+            if (headers.Exists((kvp) => kvp.Key == "X-Forwarded-For"))
+            {
+                // when running behind a load balancer you can expect this header
+                var header = headers.First((kvp) => kvp.Key == "X-Forwarded-For").Value.ToString();
+                // in case the IP contains a port, remove ':' and everything after
+                ipAddress = IPAddress.Parse(header.Remove(header.IndexOf(':')));
+            }
+            else
+            {
+                // this will always have a value (running locally in development won't have the header)
+                ipAddress = Request.HttpContext.Connection.RemoteIpAddress;
+            }
+            return ipAddress.ToString();
+        }
+    }
+}
