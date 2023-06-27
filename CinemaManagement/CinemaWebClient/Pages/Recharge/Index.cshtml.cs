2a55eef654c64d4f49131cf6385feb9a90940920
hiuhihi78 <leduchieu2001x@gmail.com>
2023-06-27 20:42:14 +0700

    add recharge screen

---
+++ CinemaManagement/CinemaWebClient/Pages/Recharge/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CinemaWebClient.Pages.Payment
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
