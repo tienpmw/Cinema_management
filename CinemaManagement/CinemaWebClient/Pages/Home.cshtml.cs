57b98c9d7f621b3fa11c7aec1659e5ae24f65704
PhiMinhTien <pmt11112001@gmail.com>
2023-07-03 15:41:55 +0700

    merge

--- CinemaManagement/CinemaWebClient/Pages/Home.cshtml.cs
+++ CinemaManagement/CinemaWebClient/Pages/Home.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Http.Headers;
 
 namespace CinemaWebClient.Pages
 {
     public class HomeModel : PageModel
     {
-        public void OnGet()
+        private readonly HttpClient _httpClient;
+        private readonly string FilmApi = string.Empty;
+
+		public HomeModel()
+		{
+			_httpClient = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            _httpClient.DefaultRequestHeaders.Accept.Add(contentType);
+            FilmApi = "http://localhost:5001/api/Films";
+		}
+
+		public async Task OnGet()
         {
+
         }
     }
 }
