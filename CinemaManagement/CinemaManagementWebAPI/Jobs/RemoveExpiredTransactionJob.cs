776eb51144a0ce80856fedf1d2588c8f13e0c016
hiuhihi78 <leduchieu2001x@gmail.com>
2023-07-03 09:32:04 +0700

    add format datetime func

---
+++ CinemaManagement/CinemaManagementWebAPI/Jobs/RemoveExpiredTransactionJob.cs
@@ -0,0 +1,13 @@
+﻿using DataAccess.DAOs;
+using Quartz;
+
+namespace CinemaWebAPI.Jobs
+{
+    public class RemoveExpiredTransactionJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            TransactionDAO.Instance.RemoveExpiredTransaction(3);
+        }
+    }
+}
