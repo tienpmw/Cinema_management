1ac54b6b3e9e0fb5794578606b3fcfc4a86f9a63
PhiMinhTien <pmt11112001@gmail.com>
2023-07-04 22:43:02 +0700

    done booking show

--- CinemaManagement/CinemaWebClient/Pages/Film/Booking/Index.cshtml.cs
+++ CinemaManagement/CinemaWebClient/Pages/Film/Booking/Index.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
 
 namespace CinemaWebClient.Pages.Film.Booking
@@ -12,6 +13,7 @@ namespace CinemaWebClient.Pages.Film.Booking
 	{
 		private readonly HttpClient _httpClient;
 		private readonly string ShowApi = string.Empty;
+		private readonly string BookingApi = string.Empty;
 
 		public IndexModel()
 		{
@@ -19,6 +21,7 @@ namespace CinemaWebClient.Pages.Film.Booking
 			var contentType = new MediaTypeWithQualityHeaderValue("application/json");
 			_httpClient.DefaultRequestHeaders.Accept.Add(contentType);
 			ShowApi = "http://localhost:5001/api/Shows";
+			BookingApi = "http://localhost:5001/api/Bookings";
 		}
 		[BindProperty]
 		public ShowDTO? Show { get; set; }
@@ -40,14 +43,14 @@ namespace CinemaWebClient.Pages.Film.Booking
 		public async Task<IActionResult> OnPost(long id)
 		{
 			string?[] seats = Request.Form["seats"].ToArray();
-			if(seats == null || seats.Length == 0)
+			if (seats == null || seats.Length == 0)
 			{
-				HttpResponseMessage response = await _httpClient.GetAsync($"{ShowApi}/{id}");
-				if (!response.IsSuccessStatusCode)
+				HttpResponseMessage response1 = await _httpClient.GetAsync($"{ShowApi}/{id}");
+				if (!response1.IsSuccessStatusCode)
 				{
 					return RedirectToPage("/Home");
 				}
-				string data = await response.Content.ReadAsStringAsync();
+				string data = await response1.Content.ReadAsStringAsync();
 				var options = new JsonSerializerOptions
 				{
 					PropertyNameCaseInsensitive = true
@@ -57,8 +60,21 @@ namespace CinemaWebClient.Pages.Film.Booking
 				ModelState.AddModelError("", "Please select seat!");
 				return Page();
 			}
-
-			return RedirectToPage("/Film/Booking/Index");
+			BookingRequestDTO booking = new BookingRequestDTO
+			{
+				UserId = 1,
+				SeatsBooking = seats
+			};
+			var content = new StringContent(JsonSerializer.Serialize(booking), Encoding.UTF8, "application/json");
+			HttpResponseMessage response = await _httpClient.PostAsync($"{BookingApi}/{id}", content);
+			if(!response.IsSuccessStatusCode)
+			{
+				TempData["ErrorMsg"] = response.Content.ReadAsStringAsync();
+			} else
+			{
+				TempData["SuccessMsg"] = "Booking seats success!";
+			}
+			return RedirectToPage("/Film/Booking/Index", new { id = id });
 		}
 	}
 }
